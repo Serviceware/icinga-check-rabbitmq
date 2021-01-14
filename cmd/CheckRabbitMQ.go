4c53946bbb46e83cca685ef5523b3369b46a9c63
Martin Krueger <martin.krueger@sabio.de>
2021-01-14 11:17:43 +0100

    read only password from file

--- cmd/CheckRabbitMQ.go
+++ cmd/CheckRabbitMQ.go
@@ -4,7 +4,6 @@ import (
 	"bitbucket.org/sabio-it/icinga-check-rabbitmq/internal"
 	"github.com/jessevdk/go-flags"
 	rabbithole "github.com/michaelklishin/rabbit-hole/v2"
-	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
 	"os"
@@ -13,23 +12,18 @@ import (
 type args struct {
 	Address string `long:"address" description:"Address of the nomad server" group:"connection"`
 
-	CaCert          string `long:"ca" description:"Path to ca cert" group:"connection"`
-	ClientCert      string `long:"cert" description:"Path to client cert" group:"connection"`
-	ClientKey       string `long:"key" description:"Path to client key" group:"connection"`
-	Username        string `long:"username" description:"Username for RabbitMQ authentication" group:"connection"`
-	Password        string `long:"password" description:"Password for RabbitMQ authentication" group:"connection"`
-	CredentialsFile string `long:"credentialsFile" description:"Path to a file which contains credentials" group:"connection"`
+	CaCert       string `long:"ca" description:"Path to ca cert" group:"connection"`
+	ClientCert   string `long:"cert" description:"Path to client cert" group:"connection"`
+	ClientKey    string `long:"key" description:"Path to client key" group:"connection"`
+	Username     string `long:"username" description:"Username for RabbitMQ authentication" group:"connection"`
+	Password     string `long:"password" description:"Password for RabbitMQ authentication" group:"connection"`
+	PasswordFile string `long:"passwordFile" description:"File which contains the password for RabbitMQ authentication" group:"connection"`
 
 	Aliveness void `command:"aliveness"`
 	Check     void `command:"check"`
 }
 
 type void struct {
-}
-
-type credentials struct {
-	Username string `yaml:"username"`
-	Password string `yaml:"password"`
 }
 
 var opts = new(args)
@@ -47,13 +41,13 @@ func main() {
 }
 
 func rabbitmqClient() *rabbithole.Client {
-	credentials := readCredentials()
+	password := readPassword()
 	config := internal.CLientConfig{
 		CaCert:     opts.CaCert,
 		ClientCert: opts.ClientCert,
 		ClientKey:  opts.ClientKey,
-		Username:   credentials.Username,
-		Password:   credentials.Password,
+		Username:   opts.Username,
+		Password:   password,
 	}
 	client, err := config.NewRabbitMQClient()
 
@@ -64,38 +58,27 @@ func rabbitmqClient() *rabbithole.Client {
 	return client
 }
 
-func readCredentials() *credentials {
-	if opts.CredentialsFile != "" {
+func readPassword() string {
+	if opts.PasswordFile != "" {
 		credentials := readCredentialsFromFile()
 
-		if credentials != nil {
+		if credentials != "" {
 			return credentials
 		}
 	}
 
-	return &credentials{
-		Username: opts.Username,
-		Password: opts.Password,
-	}
+	return opts.Password
 }
 
-func readCredentialsFromFile() *credentials {
-	data, err := ioutil.ReadFile(opts.CredentialsFile)
+func readCredentialsFromFile() string {
+	data, err := ioutil.ReadFile(opts.PasswordFile)
 
 	if err != nil {
-		println("cannot read file", opts.CredentialsFile, ":", err.Error())
-		return nil
+		println("cannot read file", opts.PasswordFile, ":", err.Error())
+		return ""
 	}
 
-	credentials := &credentials{}
-	err = yaml.Unmarshal(data, credentials)
-
-	if err != nil {
-		println("cannot read credentials from", opts.CredentialsFile, ":", err.Error())
-		return nil
-	}
-
-	return credentials
+	return string(data)
 }
 
 func parseFlags() *flags.Parser {
