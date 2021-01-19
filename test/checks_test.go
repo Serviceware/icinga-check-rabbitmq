d9cf194b0fadcc467ca4c7d7ab2c1db6ffcf8406
Martin Krueger <martin.krueger@sabio.de>
2021-01-19 13:18:10 +0100

    set node name

--- test/checks_test.go
+++ test/checks_test.go
@@ -2,26 +2,8 @@ package test
 
 import (
 	"bitbucket.org/sabio-it/icinga-check-rabbitmq/internal"
-	rabbithole "github.com/Serviceware/rabbit-hole/v2"
-	"log"
 	"testing"
 )
-
-func client() *rabbithole.Client {
-	config := internal.CLientConfig{
-		Address:  "http://localhost:15672",
-		Username: "guest",
-		Password: "guest",
-	}
-
-	client, err := config.NewRabbitMQClient()
-
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	return client
-}
 
 func TestChannels(t *testing.T) {
 	status := internal.CheckQueues(client())
@@ -53,7 +35,7 @@ func TestMessages(t *testing.T) {
 
 func TestNode(t *testing.T) {
 	opts := &internal.CheckNodeOpts{
-		Node: "rabbit@3dafefcb50fc",
+		Node: "rabbit@github",
 	}
 	status := internal.CheckNode(client(), opts)
 
---
+++ test/client.go
@@ -0,0 +1,23 @@
+package test
+
+import (
+	"bitbucket.org/sabio-it/icinga-check-rabbitmq/internal"
+	rabbithole "github.com/Serviceware/rabbit-hole/v2"
+	"log"
+)
+
+func client() *rabbithole.Client {
+	config := internal.CLientConfig{
+		Address:  "http://localhost:15672",
+		Username: "guest",
+		Password: "guest",
+	}
+
+	client, err := config.NewRabbitMQClient()
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	return client
+}
